99f80d0b48f3d4bcf5b9db95c39cbd5886a0e23f
yehor <yegorlitsov@gmail.com>
2021-03-17 13:06:28 +0200

    changes

--- unlayer-tools.js
+++ unlayer-tools.js
@@ -47,47 +47,79 @@ let commonTemplate = _.template(`
     </div>
 `);
 
+const localValues = {
+  product_image: true,
+  product_label: true,
+  product_price: true,
+  text_align: 'center'
+}
+
 unlayer.registerTool({
   name: "recommended_product",
   label: "Recommended product",
   icon: "fa-shopping-basket",
   supportedDisplayModes: ["web", "email"],
-  options: {},
+  options: {
+    align_products: {
+      title: 'Align Products',
+      defaultValue: 'center',
+      widget: 'dropdown'
+    },
+    include_image: {
+      title: 'Include Image',
+      defaultValue: true,
+      widget: 'toggle'
+    },
+    include_label: {
+      title: 'Include Label',
+      defaultValue: true,
+      widget: 'toggle'
+    },
+    include_price: {
+      title: 'Include Price',
+      defaultValue: true,
+      widget: 'toggle'
+    }
+  },
   values: {},
   renderer: {
     Viewer: unlayer.createViewer({
       render(values) {
-        const localValues = {
-          product_image: true,
-          product_label: true,
-          product_price: true,
-          text_align: 'center'
-        }
         return commonTemplate(localValues);
       }
     }),
     exporters: {
       web: function (values) {
-        const localValues = {
-          product_image: true,
-          product_label: true,
-          product_price: true,
-          text_align: 'center'
-        }
         return commonTemplate(localValues);
       },
       email: function (values) {
-        const localValues = {
-          product_image: true,
-          product_label: true,
-          product_price: true,
-          text_align: 'center'
-        }
         return commonTemplate(localValues);
       }
     },
     head: {
-      css: function (values) {},
+      css: function (values) {
+        return `
+        .pf-recommended-product {
+            text-align: <%= text_align%>;
+        }
+        .pf-recommended-product-link {
+            text-decoration: none;
+            color: black;
+            display: inline-flex;
+            flex-direction: column;
+            align-items: center;
+        }
+        .pf-recommended-product-img {
+            height: 100px;
+            width: 100px;
+            background: lightgray;
+            border: 2px dashed gray;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+        }
+        `
+      },
       js: function (values) {}
     }
   }
